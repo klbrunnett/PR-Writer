6b87a5eb3b27350c97c55d81f052fe6510deaa60
Kyle Brunnett <klbrunnett@gmail.com>
2018-12-05 23:39:54 -0800

    Try to get React to work on InputIndustry

--- homepage.js
+++ homepage.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var _get = function get(object, property, receiver) { if (object === null) object = Function.prototype; var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -20,15 +22,15 @@ var SearchHeader = function (_React$Component) {
    }
 
    _createClass(SearchHeader, [{
-      key: 'render',
+      key: "render",
       value: function render() {
          return React.createElement(
-            'div',
-            { 'class': 'col text-center' },
+            "div",
+            { "class": "col text-center" },
             React.createElement(
-               'h1',
+               "h1",
                null,
-               'Search for News'
+               "Search for News"
             )
          );
       }
@@ -37,29 +39,97 @@ var SearchHeader = function (_React$Component) {
    return SearchHeader;
 }(React.Component);
 
-var LikeButton = function (_React$Component2) {
-   _inherits(LikeButton, _React$Component2);
+var Dropdown = function (_React$Component2) {
+   _inherits(Dropdown, _React$Component2);
+
+   function Dropdown(props) {
+      _classCallCheck(this, Dropdown);
+
+      return _possibleConstructorReturn(this, (Dropdown.__proto__ || Object.getPrototypeOf(Dropdown)).call(this, props));
+   }
+
+   _createClass(Dropdown, [{
+      key: "populateDropdown",
+      value: function populateDropdown(contents) {
+         var dropdownContents = React.createElement(
+            "div",
+            { "class": "input-group" },
+            React.createElement(
+               "div",
+               { "class": "input-group-prepend" },
+               React.createElement(
+                  "label",
+                  { "class": "input-group-text", "for": "inputIndustrySelect" },
+                  "Industry"
+               )
+            ),
+            React.createElement(
+               "select",
+               { "class": "custom-select", id: "inputIndustrySelect", defaultValue: "" },
+               React.createElement(
+                  "option",
+                  { value: "" },
+                  "Choose..."
+               ),
+               "); var contentLength = contents.length; dropdownContents += ",
+               React.createElement(
+                  "option",
+                  { value: "0" },
+                  "Automotive"
+               ),
+               "; dropdownContents += ("
+            )
+         );
+         return dropdownContents;
+      }
+   }]);
+
+   return Dropdown;
+}(React.Component);
+
+var InputIndustryDropdown = function (_Dropdown) {
+   _inherits(InputIndustryDropdown, _Dropdown);
+
+   function InputIndustryDropdown(props) {
+      _classCallCheck(this, InputIndustryDropdown);
+
+      return _possibleConstructorReturn(this, (InputIndustryDropdown.__proto__ || Object.getPrototypeOf(InputIndustryDropdown)).call(this, props));
+   }
+
+   _createClass(InputIndustryDropdown, [{
+      key: "render",
+      value: function render() {
+         var inputIndustryContents = ["Agriculture", "Automotive", "Cybersecurity", "Data Technology", "Healthcare", "Financial"];
+         return _get(InputIndustryDropdown.prototype.__proto__ || Object.getPrototypeOf(InputIndustryDropdown.prototype), "populateDropdown", this).call(this, inputIndustryContents);
+      }
+   }]);
+
+   return InputIndustryDropdown;
+}(Dropdown);
+
+var LikeButton = function (_React$Component3) {
+   _inherits(LikeButton, _React$Component3);
 
    function LikeButton(props) {
       _classCallCheck(this, LikeButton);
 
-      var _this2 = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
+      var _this4 = _possibleConstructorReturn(this, (LikeButton.__proto__ || Object.getPrototypeOf(LikeButton)).call(this, props));
 
-      _this2.state = { liked: false };
-      return _this2;
+      _this4.state = { liked: false };
+      return _this4;
    }
 
    _createClass(LikeButton, [{
-      key: 'render',
+      key: "render",
       value: function render() {
-         var _this3 = this;
+         var _this5 = this;
 
          if (this.state.liked) {
             return 'You liked this.';
          }
 
          return e('button', { onClick: function onClick() {
-               return _this3.setState({ liked: true });
+               return _this5.setState({ liked: true });
             } }, 'Like');
       }
    }]);
@@ -69,3 +139,6 @@ var LikeButton = function (_React$Component2) {
 
 var domContainer = document.querySelector('#search-header');
 ReactDOM.render(e(SearchHeader), domContainer);
+
+var domContainer1 = document.querySelector('#input-industry');
+ReactDOM.render(e(InputIndustryDropdown), domContainer1);
--- src/homepage.js
+++ src/homepage.js
@@ -8,9 +8,50 @@ class SearchHeader extends React.Component {
    }
    
    render() {
-      return   <div class="col text-center">
-                  <h1>Search for News</h1>
-               </div>;
+      return (
+         <div class="col text-center">
+            <h1>Search for News</h1>
+         </div>
+      );
+   }
+}
+
+class Dropdown extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+   
+   populateDropdown(contents) {
+      var dropdownContents =  (
+         <div class="input-group">
+            <div class="input-group-prepend">
+               <label class="input-group-text" for="inputIndustrySelect">Industry</label>
+            </div>
+            <select class="custom-select" id="inputIndustrySelect" defaultValue="">
+               <option value="">Choose...</option>
+      );
+      
+      var contentLength = contents.length;
+      for (var i = 0; i < contentLength; i++) {
+         dropdownContents += <option value={i}>{contents[i]}</option>;
+      }
+      
+      dropdownContents += (
+            </select>
+         </div>
+      );
+      return dropdownContents;
+   }
+}
+
+class InputIndustryDropdown extends Dropdown {
+   constructor(props) {
+      super(props);
+   }
+   
+   render() {
+      var inputIndustryContents = [ "Agriculture", "Automotive", "Cybersecurity", "Data Technology", "Healthcare", "Financial" ];
+      return super.populateDropdown(inputIndustryContents);
    }
 }
 
@@ -35,3 +76,6 @@ class LikeButton extends React.Component {
 
 const domContainer = document.querySelector('#search-header');
 ReactDOM.render(e(SearchHeader), domContainer);
+
+const domContainer1 = document.querySelector('#input-industry');
+ReactDOM.render(e(InputIndustryDropdown), domContainer1);
